40afbc5a271c416587db130ad56c459b32e20ca3
avarnie <84094753+avarnie@users.noreply.github.com>
2022-12-09 10:24:43 +0000

    I added react to my work

---
+++ README.old.md
@@ -0,0 +1,2 @@
+# libjobs
+This is a React application where you can easily find job in Liberia.
---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>I am here</p>
+  );
+}
+
+export default App;
